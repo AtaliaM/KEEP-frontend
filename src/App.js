5ba2624648ef615786aaca17c2a1e13aebb13a27
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2022-02-23 18:18:16 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  
+  render() {
+    console.log(process.env.NODE_ENV)
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
