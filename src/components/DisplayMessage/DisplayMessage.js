762d2e574e859fd7e0d0d4638bbb03f9db404c9f
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2022-02-26 18:24:41 +0200

    delete note and display message

--- src/components/DisplayMessage/DisplayMessage.js
+++ src/components/DisplayMessage/DisplayMessage.js
@@ -5,16 +5,16 @@ class DisplayMessage extends React.Component {
 
     render() {
         return (
-            <div class="ui segment message">
-                <div class="content">
-                    <div class ="description">
+            <div className="ui segment message">
+                <div className="content">
+                    <div className ="description">
                     {this.props.message}
                     </div>
                 </div>
-                <div class="extra content">
-                    <div class="ui two buttons">
-                        <button class="circular ui icon button" onClick={this.props.onApprove}>Approve</button>
-                        <button class="circular ui icon button" onClick={this.props.onDecline}>Decline</button>
+                <div className="extra content">
+                    <div className="ui two buttons">
+                        <button className="circular ui icon button" onClick={this.props.onApprove}>Approve</button>
+                        <button className="circular ui icon button" onClick={this.props.onDecline}>Decline</button>
                     </div>
                 </div>
             </div>
